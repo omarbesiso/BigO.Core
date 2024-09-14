73e0e300cebc37cbf448b23e8b87c20082faf252
Omar Besiso <omar@cloud5.tech>
2024-09-14 13:08:05 +1000

    Refactor Create method in CultureInfoFactory class
    Updated XML documentation for the Create method to clarify the requirements for the cultureName parameter and the exceptions thrown. Added a check to throw an ArgumentException if cultureName is null, empty, or whitespace. Removed redundant variable and directly returned CultureInfo object within the try block.

--- src/BigO.Core/Factories/CultureInfoFactory.cs
+++ src/BigO.Core/Factories/CultureInfoFactory.cs
@@ -7,17 +7,28 @@ internal static class CultureInfoFactory
 {
     /// <summary>
     ///     Creates a <see cref="CultureInfo" /> object using the specified culture name.
-    ///     If the culture name is <c>null</c>, empty, or consists only of white-space characters, an
-    ///     <see cref="ArgumentNullException" /> or <see cref="ArgumentException" /> is thrown.
+    ///     If the culture name is <c>null</c>, empty, or consists only of white-space characters,
+    ///     an <see cref="ArgumentNullException" /> or <see cref="ArgumentException" /> is thrown.
     ///     If the specified culture is not found, a <see cref="CultureNotFoundException" /> is thrown.
     /// </summary>
-    /// <param name="cultureName">The name of the culture to create.</param>
-    /// <returns>A <see cref="CultureInfo" /> object representing the specified culture.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="cultureName" /> is <c>null</c>.</exception>
-    /// <exception cref="CultureNotFoundException">Thrown if the specified culture is not found.</exception>
+    /// <param name="cultureName">
+    ///     The name of the culture to create. It must be a valid culture name string that is neither null nor empty.
+    /// </param>
+    /// <returns>
+    ///     A <see cref="CultureInfo" /> object representing the specified culture.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    ///     Thrown if <paramref name="cultureName" /> is <c>null</c>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    ///     Thrown if <paramref name="cultureName" /> is empty or contains only white-space characters.
+    /// </exception>
+    /// <exception cref="CultureNotFoundException">
+    ///     Thrown if the specified culture is not found.
+    /// </exception>
     /// <remarks>
-    ///     This method is useful for creating a <see cref="CultureInfo" /> object from a culture name string,
-    ///     ensuring that the culture name is valid and the culture exists.
+    ///     This method is useful for creating a <see cref="CultureInfo" /> object from a culture name string.
+    ///     It validates that the culture name is not null, empty, or white-space, and ensures that the culture exists.
     /// </remarks>
     /// <example>
     ///     <code>
@@ -30,16 +41,19 @@ internal static class CultureInfoFactory
     {
         Guard.NotNull(cultureName);
 
-        CultureInfo culture;
+        if (string.IsNullOrWhiteSpace(cultureName))
+        {
+            throw new ArgumentException("Culture name cannot be empty or consist only of white-space characters.",
+                nameof(cultureName));
+        }
+
         try
         {
-            culture = new CultureInfo(cultureName);
+            return new CultureInfo(cultureName);
         }
         catch (CultureNotFoundException ex)
         {
             throw new CultureNotFoundException($"The culture specified by '{cultureName}' is not found.", ex);
         }
-
-        return culture;
     }
 }
